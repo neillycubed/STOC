d8b40a10f855b9615c14c4df4eb3421be8d722ac
neillycubed <neillybusiness3@gmail.com>
2021-02-10 14:59:01 -0800

    Add a reload command

--- stoc/bot.py
+++ stoc/bot.py
@@ -8,6 +8,12 @@ conf = config.load_config()
 
 client = commands.Bot(command_prefix = conf["prefix"])
 
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+    print(f'{extension} reloaded successfully!')
+
 @client.event
 async def on_ready():
     print(f'STOC v{conf["version"]} loaded successfully!')
